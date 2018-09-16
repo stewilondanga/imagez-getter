94acb902692cdb4552d4afb99b32a6e674e5f1a9
Stewart ilondanga <stilondanga@gmail.com>
2018-09-16 11:22:27 +0300

    add js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var navigate = (function() {
+	$('.dd').toggle();
+	$('.dd_btn').click(function() {
+		var dataName = $(this).attr('data-name');
+		$('.dd').hide();
+		$('.' + dataName).toggle();
+	});
+})();
